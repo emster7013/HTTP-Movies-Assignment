728354d89f9f8897c068a53f52beae3162ad1b4a
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-02-20 10:43:38 -0500

    Have added changes with delete and update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Route } from "react-router-dom";
+import axios from 'axios';
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from './Movies/UpdateMovie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => {
+        console.log('this is your movie API',res.data)
+        setMovie(res.data);
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
@@ -18,9 +32,15 @@ const App = () => {
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} movie={movie} editMovie={setMovie}  />;
         }}
       />
+      <Route 
+      path='/update-movie/:id'
+      render={props => {
+        return <UpdateMovie {...props} movies={movies} updateMovies={setMovies}/> 
+        }}
+        />
     </>
   );
 };
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialItem = {
+    title: '',
+    director: '',
+    metascore: ''
+}
+
+
+const UpdateMovie = props => {
+    const [update, setUpdate] = useState(initialItem);
+
+    useEffect(() => {
+        if (props.movies) {
+        const selectedMovie = props.movies.find(movie => {
+            return `${movie.id}` === props.match.params.id
+        });
+        console.log(selectedMovie);
+        if (selectedMovie) {
+            setUpdate(selectedMovie)
+        }
+    }
+    }, [props.movies, props.match.params.id]);
+
+    const handleChanges = e => {
+        setUpdate({
+            ...update, [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        axios
+            .put(`http://localhost:5000/api/movies/${update.id}`, update)
+            .then(res => {
+                props.updateMovies(res.data);
+            })
+            .catch(err => {
+                console.log('Update Error: ', err)
+            })
+    }
+
+    return (
+        <div className='formContainer'>
+            <form className='formBox' onSubmit={handleSubmit}>
+                <input 
+                type='text'
+                name='title'
+                placeholder='Movie Title'
+                onChange={handleChanges}
+                value={update.title}
+                />
+                <input 
+                type='text'
+                name='director'
+                placeholder='Director'
+                onChange={handleChanges}
+                value={update.director}
+                />
+                <input 
+                type='text'
+                name='metascore'
+                placeholder='MetaScore'
+                onChange={handleChanges}
+                value={update.metascore}
+                />
+                <button>Update Movie</button>
+            </form>
+        </div>
+    )
+}
+
+export default UpdateMovie;
